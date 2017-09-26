5c261bec2586becde87f6f01a3a02e1b7a1f4d9a
kgrvr <kunshgrover@gmail.com>
2017-09-26 10:35:36 +0530

    App component own css removed; Module ID added to app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   title = 'app';
