5bfd63a791c0e918c3a11eeade7ebcab40024c8d
kgrvr <kunshgrover@gmail.com>
2017-10-07 12:56:35 +0530

    About me page modified

--- src/app/back/about-me/about-me.component.ts
+++ src/app/back/about-me/about-me.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'app-about-me',
-  templateUrl: 'about-me.component.html'
+  templateUrl: 'about-me.component.html',
+  styleUrls: ['about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
 
